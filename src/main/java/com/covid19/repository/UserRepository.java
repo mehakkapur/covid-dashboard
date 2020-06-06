fcfe4d288c37052a62f4264609242edd5ba8dcb5
mehakkapur <mehakkapur90@gmail.com>
2020-06-06 15:33:53 +0530

    UserRepository

---
+++ src/main/java/com/covid19/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.covid19.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.covid19.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+	User findByUsername(String username);
+	}
+
